b9a848f282ef6aecb0d889a84e0d31d4be85872b
Pollapee <im.oab.ps@gmail.com>
2022-12-12 15:48:54 +1100

    fixed benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -7,7 +7,9 @@ pub mod Y2022;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("Day_01", |b| b.iter(|| crate::Y2022::days::day_01::Day01::run()));
+    c.bench_function("Day_01", |b| {
+        b.iter(|| crate::Y2022::days::day_01::Day01::run())
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
