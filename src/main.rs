62b0c115c4f4c2e70d7eea35c1fce215e350c518
Pollapee <im.oab.ps@gmail.com>
2022-12-12 15:33:03 +1100

    updated code to match new folder structure.

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,20 @@ pub mod file_handler;
 pub mod Y2022;
 
 fn main() {
-    // print_result(crate::days::day_01::Day01::run());
-    // print_result(crate::days::day_02::Day02::run());
-    // print_result(crate::days::day_03::Day03::run());
-    // print_result(crate::days::day_04::Day04::run());
-    // print_result(crate::days::day_05::Day05::run());
-    // print_result(crate::days::day_06::Day06::run());
-    // print_result(crate::days::day_07::Day07::run());
-    // print_result(crate::days::day_08::Day08::run());
-    // print_result(crate::days::day_09::Day09::run());
-    // print_result(crate::days::day_10::Day10::run());
-    // print_result(crate::days::day_11::Day11::run());
-    print_result(crate::Y2022::days::day_12::Day12::run());
+    
+
+    // print_result(crate::Y2022::days::day_01::Day01::run());
+    // print_result(crate::Y2022::days::day_02::Day02::run());
+    // print_result(crate::Y2022::days::day_03::Day03::run());
+    // print_result(crate::Y2022::days::day_04::Day04::run());
+    // print_result(crate::Y2022::days::day_05::Day05::run());
+    // print_result(crate::Y2022::days::day_06::Day06::run());
+    // print_result(crate::Y2022::days::day_07::Day07::run());
+    // print_result(crate::Y2022::days::day_08::Day08::run());
+    // print_result(crate::Y2022::days::day_09::Day09::run());
+    // print_result(crate::Y2022::days::day_10::Day10::run());
+    // print_result(crate::Y2022::days::day_11::Day11::run());
+    // print_result(crate::Y2022::days::day_12::Day12::run());
 }
 
 fn print_result(result: (String, String, String, u128, u128)) {
