38e17bfa0e6ce815b11b06e31aec9cbe682e8e77
Pollapee <im.oab.ps@gmail.com>
2023-12-02 15:59:54 +1100

    prepare for y2023/day02

---
+++ src/Y2023/days/day_02.rs
@@ -0,0 +1,51 @@
+use std::time::Instant;
+
+use crate::file_handler::FileHandler;
+
+pub struct Day02 {}
+
+impl Day02 {
+    pub fn run() -> (String, String, String, u128, u128) {
+        let first_input = FileHandler::read("./src/Y2023/inputs/day_02_1.txt");
+
+        let lines: Vec<&str> = first_input.split('\n').collect();
+
+        let start_1 = Instant::now();
+        let result_1 = Day02::part_01(&lines);
+        let time_calculation_1 = start_1.elapsed();
+
+        let start_2 = Instant::now();
+        let result_2 = Day02::part_02(&lines);
+        let time_calculation_2 = start_2.elapsed();
+
+        (
+            "Day_01".to_owned(),
+            format!("{}", result_1),
+            format!("{}", result_2),
+            time_calculation_1.as_nanos(),
+            time_calculation_2.as_nanos(),
+        )
+    }
+
+    fn part_01(lines: &Vec<&str>) -> i32 {
+        0
+    }
+
+    fn part_02(lines: &Vec<&str>) -> i32 {
+        0
+    }
+}
+const TEST_INPUT: &str = "";
+
+const TEST_INPUT_2: &str = "";
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {}
+
+    #[test]
+    fn test_part_2() {}
+}
